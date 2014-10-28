e4e89a4ee3c19cf86d549a1219af6ad6dd50b079
Matthew Pocock <turingatemyhamster@gmail.com>
2014-10-28 16:06:40 +0000

    Tidying imports and eliminating dead commented code.

--- scalajs/src/main/scala/sbolv/GlyphFamily.scala
+++ scalajs/src/main/scala/sbolv/GlyphFamily.scala
@@ -1,10 +1,7 @@
 package sbolv
 
-import java.util.Random
-
 import org.scalajs.dom._
-import rx.core.{Var, Obs, Rx}
-import sbolv.geom.{Box}
+import rx.core.Rx
 
 import scalatags.JsDom.implicits._
 import scalatags.JsDom.svgAttrs._
@@ -130,107 +127,3 @@ object BoxyGlyph {
     override final def geometryToBaseline(g: Geometry) = g.bot
   }
 }
-
-//trait GlyphFamilyWithInnerLabel {
-//  self : GlyphFamily =>
-//
-//  import scalatags.JsDom.implicits._
-//  import scalatags.JsDom.svgTags._
-//  import scalatags.JsDom.svgAttrs._
-//  import Framework._
-//
-//  def innerLabel: Rx[Option[String]]
-//
-//  private val innerLabelTick = Var(0)
-//  private def tickInner() = innerLabelTick() = innerLabelTick() + 1
-//
-//  private val innerLabelClean = Rx {
-//    innerLabel() getOrElse ""
-//  }
-//
-//  private lazy val innerLabelText_transform = Var("translate(0 0)")
-//
-//  protected final val innerLabelText = {
-//    val txt = text(
-//      `class` := "sbolv_glyph_label",
-//      transform := (innerLabelText_transform : Rx[String]),
-//      DOMNodeInsertedIntoDocument := { (e: Event) =>
-//        tickInner() },
-//      DOMNodeInserted := { (e: Event) =>
-//        tickInner() },
-//      DOMSubtreeModified := { (e: Event) =>
-//        tickInner() }
-//    )(innerLabelClean).render
-//
-//
-//    val textBox = Rx {
-//      innerLabelTick()
-//      innerLabelClean()
-//      Box(txt.getBBox())
-//    }
-//
-//    val innerLabelText_transform_obs = Obs(textBox) {
-//      val bounds = textBox()
-//      val centre = bounds.centre
-//      innerLabelText_transform() = s"translate(${-centre.x} ${-centre.y})"
-//    }
-//
-//    txt
-//  }
-//
-//}
-//
-//trait GlyphFamilyWithOuterLabel {
-//  self : GlyphFamily =>
-//
-//  import scalatags.JsDom.implicits._
-//  import scalatags.JsDom.svgTags._
-//  import scalatags.JsDom.svgAttrs._
-//  import Framework._
-//
-//  def outerLabel: Rx[Option[String]]
-//  protected def offset: Rx[Double]
-//
-//  private val outerLabelTick = Var(0)
-//  private def tickOuter() = outerLabelTick() = outerLabelTick() + 1
-//
-//  private lazy val outerLabelClean = Rx {
-//    outerLabel() getOrElse ""
-//  }
-//
-//  private lazy val outerLabelText_transform = Var("translate(0 0)")
-//
-//  protected final val outerLabelText = {
-//    val txt = text(
-//      `class` := "sbolv_glyph_label",
-//      transform := (outerLabelText_transform : Rx[String]),
-//      DOMNodeInsertedIntoDocument := { (e: Event) => tickOuter() },
-//      DOMNodeInserted := { (e: Event) => tickOuter() },
-//      DOMSubtreeModified := { (e: Event) => tickOuter() }
-//    )(outerLabelClean).render
-//
-//    val textBox = Rx {
-//      outerLabelTick()
-//      outerLabelClean()
-//      Box(txt.getBBox())
-//    }
-//
-//    val trans = Rx {
-//      val bounds = textBox()
-//      val centre = bounds.centre
-//      val m = metrics()
-//      val o = offset()
-//      alignment() match {
-//        case AboveBackbone => s"translate(${-centre.x} ${+o +bounds.bottom})"
-//        case CentredOnBackbone => s"translate(${-centre.x} ${+o +bounds.bottom})"
-//        case BelowBackbone => s"translate(${-centre.x} ${+o +bounds.bottom})"
-//      }
-//    }
-//
-//    val transform_obs = Obs(trans) {
-//      outerLabelText_transform() = trans()
-//    }
-//
-//    txt
-//  }
-//}
